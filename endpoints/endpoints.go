2759a8fb2f2f8e2f2fb23b7e07a2c4c0997bda94
jzunigax2 <125698953+jzunigax2@users.noreply.github.com>
2025-11-28 16:01:37 -0600

    chore: remove unused endpoints after cleanup

--- endpoints/endpoints.go
+++ endpoints/endpoints.go
@@ -64,24 +64,9 @@ func (d *DriveEndpoints) Folders() *FolderEndpoints {
 	return &FolderEndpoints{base: d.base + "/folders"}
 }
 
-// Trash returns trash-related endpoints
-func (d *DriveEndpoints) Trash() *TrashEndpoints {
-	return &TrashEndpoints{base: d.base + "/storage/trash"}
-}
-
 // Users returns user-related endpoints
 func (d *DriveEndpoints) Users() *UserEndpoints {
 	return &UserEndpoints{base: d.base + "/users"}
-}
-
-// Workspaces returns the workspaces endpoint
-func (d *DriveEndpoints) Workspaces() string {
-	return d.base + "/workspaces"
-}
-
-// FuzzySearch returns the fuzzy search endpoint
-func (d *DriveEndpoints) FuzzySearch(term string, offset int) string {
-	return fmt.Sprintf("%s/fuzzy/%s?offset=%d", d.base, url.PathEscape(term), offset)
 }
 
 // AuthEndpoints : endpoints under /drive/auth
@@ -91,8 +76,6 @@ type AuthEndpoints struct {
 
 func (a *AuthEndpoints) Login() string       { return a.base + "/login" }
 func (a *AuthEndpoints) LoginAccess() string { return a.base + "/login/access" }
-func (a *AuthEndpoints) Logout() string      { return a.base + "/logout" }
-func (a *AuthEndpoints) TFA() string         { return a.base + "/tfa" }
 
 func (a *AuthEndpoints) CredentialsCorrect(hashedPassword string) string {
 	return fmt.Sprintf("%s/are-credentials-correct?hashedPassword=%s", a.base, url.QueryEscape(hashedPassword))
@@ -104,32 +87,16 @@ type FileEndpoints struct {
 }
 
 func (f *FileEndpoints) Create() string            { return f.base }
-func (f *FileEndpoints) Recents() string           { return f.base + "/recents" }
 func (f *FileEndpoints) Meta(uuid string) string   { return f.base + "/" + uuid + "/meta" }
 func (f *FileEndpoints) Delete(uuid string) string { return f.base + "/" + uuid }
-func (f *FileEndpoints) Move(uuid string) string   { return f.base + "/" + uuid }
-func (f *FileEndpoints) ByUUID(uuid string) string { return f.base + "/" + uuid }
 
 // FolderEndpoints : endpoints under /drive/folders
 type FolderEndpoints struct {
 	base string
 }
 
-func (f *FolderEndpoints) Create() string               { return f.base }
-func (f *FolderEndpoints) Delete(uuid string) string    { return f.base + "/" + uuid }
-func (f *FolderEndpoints) Size(uuid string) string      { return f.base + "/" + uuid + "/size" }
-func (f *FolderEndpoints) Meta(uuid string) string      { return f.base + "/" + uuid + "/meta" }
-func (f *FolderEndpoints) Move(uuid string) string      { return f.base + "/" + uuid }
-func (f *FolderEndpoints) Tree(uuid string) string      { return f.base + "/" + uuid + "/tree" }
-func (f *FolderEndpoints) Ancestors(uuid string) string { return f.base + "/" + uuid + "/ancestors" }
-func (f *FolderEndpoints) MetadataByID(id int64) string {
-	return fmt.Sprintf("%s/%d/metadata", f.base, id)
-}
-func (f *FolderEndpoints) ByUUID(uuid string) string { return f.base + "/" + uuid }
-
-func (f *FolderEndpoints) MetadataByPath(path string) string {
-	return fmt.Sprintf("%s/meta?path=%s", f.base, url.QueryEscape(path))
-}
+func (f *FolderEndpoints) Create() string            { return f.base }
+func (f *FolderEndpoints) Delete(uuid string) string { return f.base + "/" + uuid }
 
 func (f *FolderEndpoints) ContentFolders(parentUUID string) string {
 	return fmt.Sprintf("%s/content/%s/folders", f.base, parentUUID)
@@ -137,21 +104,6 @@ func (f *FolderEndpoints) ContentFolders(parentUUID string) string {
 
 func (f *FolderEndpoints) ContentFiles(parentUUID string) string {
 	return fmt.Sprintf("%s/content/%s/files", f.base, parentUUID)
-}
-
-// TrashEndpoints : endpoints under /drive/storage/trash
-type TrashEndpoints struct {
-	base string
-}
-
-func (t *TrashEndpoints) Paginated() string               { return t.base + "/paginated" }
-func (t *TrashEndpoints) Add() string                     { return t.base + "/add" }
-func (t *TrashEndpoints) DeleteAll() string               { return t.base + "/all" }
-func (t *TrashEndpoints) DeleteAllRequest() string        { return t.base + "/all/request" }
-func (t *TrashEndpoints) DeleteItems() string             { return t.base }
-func (t *TrashEndpoints) DeleteFile(fileID string) string { return t.base + "/file/" + fileID }
-func (t *TrashEndpoints) DeleteFolder(folderID int64) string {
-	return fmt.Sprintf("%s/folder/%d", t.base, folderID)
 }
 
 // UserEndpoints : endpoints under /users
